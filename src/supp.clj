de5d4aa497c434d55308155f1bf0587b31a91eb3
Nada Amin <namin@alum.mit.edu>
2013-03-03 13:04:36 +0100

    better type constraints

--- src/supp.clj
+++ src/supp.clj
@@ -1,10 +1,27 @@
 (ns supp
+  (:use [live] :reload)
   (:refer-clojure :exclude [==])
   (:use [clojure.core.logic :exclude [is] :as l]
         [clojure.core.logic.nominal :exclude [fresh hash] :as nom])
   (:use [clojure.test]))
 
-;; type tests
+;;; prettier reification for single-variable constraints
+(defn reifier-for [tag x]
+  (fn [c v r a] `(~tag ~(walk* r (walk* a x)))))
 
-(defn nomo [x] (predc x nom? `nom?))
-(defn symbolo [x] (predc x symbol? `symbol?))
+;;; type constraints
+(defn nomo [x] (predc x nom? (reifier-for 'nom x)))
+
+(about "nomo"
+  (eg
+    (run* [q] (nomo q))
+    ==> '((_0 :- (nom _0))))
+)
+
+(defn symbolo [x] (predc x symbol? (reifier-for 'sym x)))
+
+(about "symbolo"
+  (eg
+    (run* [q] (symbolo q))
+    ==> '((_0 :- (sym _0))))
+)
